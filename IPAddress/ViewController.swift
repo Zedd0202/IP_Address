1ba12be2117731d63ee18c43f4386f1099f2b227
Zedd0202 <ch41150@hanmail.net>
2020-09-21 13:43:14 +0900

    first commit

---
+++ IPAddress/ViewController.swift
@@ -0,0 +1,199 @@
+//
+//  ViewController.swift
+//  IPAddress
+//
+//  Created by Zedd on 2020/09/21.
+//  Copyright © 2020 Zedd. All rights reserved.
+//
+
+import UIKit
+import SystemConfiguration
+
+struct ReachabilityStatus {
+    
+    enum status {
+        case notReachable
+        case reachableViaWWAN
+        case reachableViaWiFi
+    }
+    
+    var currentReachabilityStatus: status {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return .notReachable
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return .notReachable
+        }
+        
+        if flags.contains(.reachable) == false {
+            // The target host is not reachable.
+            return .notReachable
+        }
+        else if flags.contains(.isWWAN) == true {
+            // WWAN connections are OK if the calling application is using the CFNetwork APIs.
+            return .reachableViaWWAN
+        }
+        else if flags.contains(.connectionRequired) == false {
+            // If the target host is reachable and no connection is required then we'll assume that you're on Wi-Fi...
+            return .reachableViaWiFi
+        }
+        else if (flags.contains(.connectionOnDemand) == true || flags.contains(.connectionOnTraffic) == true) && flags.contains(.interventionRequired) == false {
+            // The connection is on-demand (or on-traffic) if the calling application is using the CFSocketStream or higher APIs and no [user] intervention is needed
+            return .reachableViaWiFi
+        }
+        else {
+            return .notReachable
+        }
+    }
+    
+    func getIPAddress() -> String {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next }
+
+                guard let interface = ptr?.pointee else { return "" }
+                let addrFamily = interface.ifa_addr.pointee.sa_family
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+
+                    // wifi = ["en0"]
+                    // wired = ["en2", "en3", "en4"]
+                    // cellular = ["pdp_ip0","pdp_ip1","pdp_ip2","pdp_ip3"]
+
+                    let name: String = String(cString: (interface.ifa_name))
+                    if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        return address ?? ""
+    }
+    
+    func getAddress(for network: Network) -> String? {
+        var address: String?
+
+        // Get list of all interfaces on the local machine:
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return nil }
+        guard let firstAddr = ifaddr else { return nil }
+
+        // For each interface ...
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+
+            // Check for IPv4 or IPv6 interface:
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+
+                // Check interface name:
+                let name = String(cString: interface.ifa_name)
+                if name == network.rawValue {
+
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname) + " \(name)"
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+
+        return address
+    }
+    enum Network: String {
+        case wifi = "en0"
+        case cellular = "pdp_ip0"
+    
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var address2: UILabel!
+    @IBOutlet weak var address1: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let status = ReachabilityStatus()
+
+        switch status.currentReachabilityStatus {
+        case .reachableViaWiFi:
+            self.address1.text = status.getAddress(for: .wifi)
+        case .reachableViaWWAN:
+            self.address1.text = status.getAddress(for: .cellular)
+        default:
+            break
+        }
+        self.address2.text = UIDevice().ipAddress()
+    }
+}
+
+extension UIDevice {
+
+    private struct InterfaceNames {
+        static let wifi = ["en0"]
+        static let wired = ["en2", "en3", "en4"]
+        static let cellular = ["pdp_ip0", "pdp_ip1", "pdp_ip2", "pdp_ip3"]
+        static let supported = wifi + wired + cellular
+    }
+
+    func ipAddress() -> String? {
+        var ipAddress: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+
+        if getifaddrs(&ifaddr) == 0 {
+            var pointer = ifaddr
+
+            while pointer != nil {
+                defer { pointer = pointer?.pointee.ifa_next }
+
+                guard
+                    let interface = pointer?.pointee,
+                    interface.ifa_addr.pointee.sa_family == UInt8(AF_INET) || interface.ifa_addr.pointee.sa_family == UInt8(AF_INET6),
+                    let interfaceName = interface.ifa_name,
+                    let interfaceNameFormatted = String(cString: interfaceName, encoding: .utf8),
+                    InterfaceNames.supported.contains(interfaceNameFormatted)
+                    else { continue }
+
+                var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+
+                getnameinfo(interface.ifa_addr,
+                            socklen_t(interface.ifa_addr.pointee.sa_len),
+                            &hostname,
+                            socklen_t(hostname.count),
+                            nil,
+                            socklen_t(0),
+                            NI_NUMERICHOST)
+
+                guard
+                    let formattedIpAddress = String(cString: hostname, encoding: .utf8),
+                    !formattedIpAddress.isEmpty
+                    else { continue }
+
+                ipAddress = formattedIpAddress + " \(interfaceNameFormatted)"
+                break
+            }
+
+            freeifaddrs(ifaddr)
+        }
+
+        return ipAddress
+    }
+}
